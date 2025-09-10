8eba60d252040840f32e23ed14d80565494b6967
JavKabPro <javierdelgado1139633@correo.itm.edu.co>
2025-09-10 15:54:13 -0500

    First test... first rule

---
+++ Workshop3_POO_logic/29_Bridges_of_Madison/Program.cs
@@ -0,0 +1,23 @@
+﻿var itString = string.Empty;
+
+do
+{
+    Console.Write("Ingrese puente (0 'salir para terminar): ");
+    itString = Console.ReadLine();
+    if (itString!.ToLower() == "salir")
+        break;
+    
+    bool isValid = itString.Length >= 2 && itString.StartsWith("*")&& itString.EndsWith("*");
+    
+    if (isValid)
+    {
+        Console.WriteLine($"El puente {itString}, es VALIDO.");
+    }
+    else
+    {
+        Console.WriteLine($"El número {itString}, es INVALIDO.");
+    }
+      
+} while (true);
+Console.WriteLine("Game Over.");
+    
