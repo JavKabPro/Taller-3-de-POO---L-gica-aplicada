9a55b70b1eb2db9c3b19aa4278a60a5a4aa9f563
JavKabPro <javierdelgado1139633@correo.itm.edu.co>
2025-09-10 16:55:46 -0500

    Four rules are run succesfully but the code is too long.

--- Workshop3_POO_logic/29_Bridges_of_Madison/Program.cs
+++ Workshop3_POO_logic/29_Bridges_of_Madison/Program.cs
@@ -9,16 +9,74 @@ do
     if (itString!.ToLower() == "salir")
         break;
     
-    bool isValid = itString.Length >= 2 && itString.StartsWith("*")&& itString.EndsWith("*");
+    bool isValid = itString.Length >= 2 && itString.StartsWith("*")&& itString.EndsWith("*");   //It must start and end with '*'.
     if (isValid)
     {
-        string inside = itString.Substring(1, itString.Length - 2);
-        if (inside.Contains("*")) 
+        string inside = itString.Substring(1, itString.Length - 2);     //Get and verify the inside part of the string.
+        if (inside.Contains("*"))                                                               //It must not contain '*'.
         {
             isValid = false;
         }
     }
-    
+    if (isValid)
+    {
+        string inside = itString.Substring(1, itString.Length - 2);     //Get the inside part of the string again.
+        for (int i = 0; i < inside.Length - 1; i++)                     //Iterate for, through the inside part of the string.                                 
+        {
+            if (inside[i] == '=' && inside[i+1] =='=')
+            {
+                bool lOk = false;
+                bool rOk = false;
+                if (i > 0 && inside[i - 1] == '+')
+                {
+                    lOk = true;
+                }
+                if (i + 2 < inside.Length && inside[i + 2] == '+')
+                {
+                    rOk = true;
+                }
+                if (!(lOk || rOk))
+                {
+                    isValid = false;
+                    break;                                              //Unnecessary to continue checking.
+                }
+            }
+        }
+    }
+    if (isValid)
+    {
+        string inside = itString.Substring(1, itString.Length - 2);
+        int countRef = 0;
+        int mid = inside.Length / 2;
+        for (int i = 0; i < inside.Length - 2; i++)
+        {
+            if (inside[i] == '=' && inside[i + 1] == '=' && inside[i + 2] == '=')
+            {
+                countRef++;
+                if (i != mid - 1)
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+        }
+        if (countRef > 1)
+        {
+            isValid = false;
+        }
+    }
+    if (isValid)
+    {
+        string inside = itString.Substring(1, itString.Length - 2);
+        for (int i = 0; i < inside.Length / 2; i++)
+        {
+            if (inside[i] != inside[inside.Length - 1 - i])
+            {
+                isValid = false;
+                break;
+            }
+        }
+    }
     if (isValid)
     {
         Console.WriteLine($"El puente {itString}, es VALIDO.");
@@ -27,7 +85,6 @@ do
     {
         Console.WriteLine($"El puente {itString}, es INVALIDO.");
     }
-
 } while (true);
 Console.WriteLine("Game Over.");
     
