2741469095c10113e99f36665672969db04fe89c
JavKabPro <javierdelgado1139633@correo.itm.edu.co>
2025-09-11 12:53:23 -0500

    New updated, comments are organized in exercise Bridges and exercise 36 begins by harvesting horses.

--- Workshop3_POO_logic/29_Bridges_of_Madison/Program.cs
+++ Workshop3_POO_logic/29_Bridges_of_Madison/Program.cs
@@ -3,32 +3,32 @@ var itString = string.Empty;                                        //Initialize
 do
 {
     Console.Write("Ingrese puente (o pulse 'salir' para terminar): ");
-    itString = Console.ReadLine();                                  //Read the input string.
-    if (itString!.ToLower() == "salir")                             //If the input is "salir", exit the loop.
+    itString = Console.ReadLine();                                                              //Read the input string.
+    if (itString!.ToLower() == "salir")                                                         //If the input is "salir", exit the loop.
         break;
     
     bool isValid = itString.Length >= 2 && itString.StartsWith("*")&& itString.EndsWith("*");   //It must start and end with '*'.
-    string inside = string.Empty;                                       //If the first a part of first rule pass, continue. (1.Rule)   
+    string inside = string.Empty;                                           //If the first a part of first rule pass, continue. (1.Rule)   
     if (isValid)
-        inside = itString.Substring(1, itString.Length - 2);            //Get and verify the inside part of the string.
+        inside = itString.Substring(1, itString.Length - 2);                //Get and verify the inside part of the string.
         
-        if (inside.Contains("*"))                                       //It must not contain '*'.
+        if (inside.Contains("*"))                                           //It must not contain '*'.
             isValid = false;
    
         if (isValid && inside.Contains("*"))
-            isValid = false;                                            //It must not contain '*'.
+            isValid = false;                                                //It must not contain '*'.
     if (isValid)
     {
-        for (int i = 0; i < inside.Length - 1; i++)                 //Iterate for, through the inside part of the string. (2. rulle)                                
+        for (int i = 0; i < inside.Length - 1; i++)                         //Iterate for, through the inside part of the string. (2. rulle)                                
         {
-            if (inside[i] == '=' && inside[i + 1] == '=')                //If there are two '=' in a row, it is invalid.
+            if (inside[i] == '=' && inside[i + 1] == '=')                   //If there are two '=' in a row, it is invalid.
             {
-                bool lOk = (i > 0 && inside[i - 1] == '+'); ;           //Check if there is a '+' to the left.
+                bool lOk = (i > 0 && inside[i - 1] == '+'); ;               //Check if there is a '+' to the left.
                 bool rOk = (i + 2 < inside.Length && inside[i + 2] == '+'); //Verify if there is a '+' to the right.
-                if (!(lOk || rOk))                                      //If there is no '+' on either side, it is invalid.
+                if (!(lOk || rOk))                                          //If there is no '+' on either side, it is invalid.
                 {
                     isValid = false;
-                    break;                                              //Unnecessary to continue checking.
+                    break;                                                  //Unnecessary to continue checking.
                 }
             }
 
@@ -36,14 +36,14 @@ do
     }
     if (isValid)
     {
-        int countRef = 0;                               //Count the number of '===' references.
-        int mid = inside.Length / 2;                    //Calculate the middle.
-        for (int i = 0; i < inside.Length - 2; i++)     //Iterate through the inside part of the string. (3.Rule)
+        int countRef = 0;                                                           //Count the number of '===' references.
+        int mid = inside.Length / 2;                                                //Calculate the middle.
+        for (int i = 0; i < inside.Length - 2; i++)                                 //Iterate through the inside part of the string. (3.Rule)
         {
             if (inside[i] == '=' && inside[i + 1] == '=' && inside[i + 2] == '=')   //The reference === must be in the middle.
             {
-                countRef++;                             //Count the number of references.
-                if (i != mid - 1)                       //If the reference is not in the middle, it is invalid.
+                countRef++;                                                         //Count the number of references.
+                if (i != mid - 1)                                                   //If the reference is not in the middle, it is invalid.
                 {
                     isValid = false;
                     break;
---
+++ Workshop3_POO_logic/36_Harvesting_on_horseback/Program.cs
@@ -0,0 +1,16 @@
+﻿class HarvestingOnHorseback
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese ubicación de los frutos: ");     //Input the location of the fruits.
+        string pos = Console.ReadLine();
+        Console.Write("Ingrese ubicación del caballo: ");       //Input the location of the horse in the chessboard.
+        string horse = Console.ReadLine();
+        Console.Write("Ingrese los movimientos de caballo: ");  //Input the horse moves. Put UR, UL, DR, DL, RU, RD, LU, LD...
+        string moves = Console.ReadLine();
+
+        char[,] chess = new char[8, 8];                         //Create an 8x8 chessboard.
+
+
+    }
+}
